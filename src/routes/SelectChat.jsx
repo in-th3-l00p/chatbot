1674d4c4fcd90355ed784ecdc0a5e65597bda9b7
in-th3-l00p <indianucode0303@gmail.com>
2023-11-29 08:30:45 +0200

    added chat functionality

--- src/routes/SelectChat.jsx
+++ src/routes/SelectChat.jsx
@@ -1,6 +1,8 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 const SelectChat = () => {
+    const [loading, setLoading] = useState(false);
     const [messages, setMessages] = useState([
         {
             sender: "user",
@@ -27,6 +29,49 @@ const SelectChat = () => {
 
             <form onSubmit={(e) => {
                 e.preventDefault();
+
+                const data = new FormData(e.currentTarget);
+                const query = data.get("query");
+
+                setLoading(true);
+                axios.post("https://api.openai.com/v1/chat/completions", {
+                    "model": "gpt-3.5-turbo",
+                    "messages": [
+                        {
+                            "role": "system",
+                            "content": "you are a senior javascript developer, your name is NexoTalk"
+                        },
+                        ...(messages
+                            .filter(message => message.sender === "user")
+                            .map(message => ({
+                                "role": "user",
+                                "content": message.content
+                            }))),
+                        {
+                            "role": "user",
+                            "content": query
+                        }
+                    ]
+                } , {
+                    headers: {
+                        "Authorization": "Bearer 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",
+                        "Accept": "application/json"
+                    }
+                })
+                    .then(resp => {
+                        setMessages(messages => ([
+                            ...messages,
+                            {
+                                sender: "user",
+                                content: query
+                            },
+                            {
+                                sender: "ai",
+                                content: resp.data.choices[0].message.content
+                            }
+                        ]))
+                        setLoading(false);
+                    })
             }}>
                 <label htmlFor="query">Enter your promt:</label>
                 <input type="text" name="query" id="query" />
